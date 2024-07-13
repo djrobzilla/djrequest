05136426d475f1aedba72597a7cb603416add446
djrobzilla <robertwaelder@gmail.com>
2024-07-13 09:53:23 -0400

    adding import for url_parse to resolve dependency and adding CNAME record file

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.urls import url_parse
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
