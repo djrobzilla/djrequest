16aac0b5d74d41713391ab545ba8148e5f1a1efb
djrobzilla <robertwaelder@gmail.com>
2024-07-24 09:56:57 -0400

    changing password hash to 200 characters to accomodate hashing requirements

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), index=True,
                          unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
     tracks = db.relationship('Track', backref='user',
                              lazy=True, foreign_keys='Track.user_id')
